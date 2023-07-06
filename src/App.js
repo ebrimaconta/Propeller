22438bf6d5bb4d7d83418bd3290e43a87c3a51ae
Ebrima Conta <ebrimaconta@gmail.com>
2023-07-06 13:45:17 +0100

    Made a change in the package json files

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
     };
     getCharacters();
   }, [state.page]);
-  console.log('state', state);
+
   const changePage = (page) => {
     if (page !== state.page) {
       setState((prevState) => ({ ...prevState, page }));
